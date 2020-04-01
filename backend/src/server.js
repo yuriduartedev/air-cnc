0c6b45673704d062004f763c2e06f9fa1cf4a947
Yuri Duarte <yuriduartedev@gmail.com>
2020-04-01 11:40:43 -0300

    Finaliza o backend

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect(
+  'mongodb+srv://yuriduarte:pEOeVPii319YNuhQ@omnistack-8lgu2.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+)
+// GET, POST, PUT, DELETE
+
+// req.query = Acessar informações enviadas através da url Ex: base_url/users?idade=20
+// req.params = Acessar route params (para edição) Ex: base_url/id
+// req.body = Acessar o corpo da requisição (para criação e edição de registros.)
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
