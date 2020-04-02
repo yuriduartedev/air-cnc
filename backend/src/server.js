a5fd701c5f3e35218bb93dc8bf3b21cdc0cb5e74
Yuri Duarte <yuriduartedev@gmail.com>
2020-04-02 12:22:34 -0300

    Finaliza frontend da aplicação

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -17,7 +19,9 @@ mongoose.connect(
 // req.params = Acessar route params (para edição) Ex: base_url/id
 // req.body = Acessar o corpo da requisição (para criação e edição de registros.)
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
