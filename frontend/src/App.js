a0cec160c332ae7f4d9f902ff0f36dbcaa2da8c2
Yuri Duarte <yuriduartedev@gmail.com>
2020-04-02 12:22:08 -0300

    Finaliza frontend da aplicação

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './App.css';
+
+import logo from './assets/logo.svg';
+
+import Routes from './routes';
+
+function App() {
+
+
+  return (
+    <div className="container">
+
+        <a href="/">
+          <img src={logo} alt="AirCnC logo"/>
+        </a>
+
+
+      <div className="content">
+        <Routes />
+      </div>
+    </div>
+  );
+}
+
+export default App;
