b823383ed48bae26ff9babc6fe6e5ad130b06e9d
Yuri Duarte <yuriduartedev@gmail.com>
2020-04-03 12:33:34 -0300

    Finaliza versão Mobile

---
+++ mobile/src/pages/List.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, ScrollView, Text, StyleSheet, AsyncStorage, Image } from 'react-native';
+
+import logo from '../assets/logo.png'
+
+import SpotList from '../components/SpotList';
+
+export default function List() {
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('techs')
+      .then(storageTechs => {
+        const techsArray = storageTechs.split(',').map(tech => tech.trim());
+
+        setTechs(techsArray)
+      })
+  }, [])
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+
+      <ScrollView>
+        {techs.map(tech => (
+          <SpotList key={tech} tech={tech} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+const styles= StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  logo: {
+    height: 32,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    marginTop: 10
+  }
+});
